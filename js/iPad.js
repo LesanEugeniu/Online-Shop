0b5f50da72d8b050f48df86faa0b61fb7465985e
Eugeniu <eugeniu1337@gmail.com>
2022-05-28 19:33:05 +0300

    Initial commit

---
+++ js/iPad.js
@@ -0,0 +1,12 @@
+var initialSrc = "../assets/iPadPro-close.png";
+var scrollSrc = "../assets/iPadpro-bkg.gif";
+let k=0;
+$(window).scroll(function() {
+   var value = $(this).scrollTop();
+   if (value > 100 && k==0){
+      $(".iPadPro-Image").attr("src", scrollSrc);
+      k++;
+    }
+//    else
+//       $(".iPadPro-Image").attr("src", initialSrc);
+});
---
+++ js/iphoneSE-local.js
@@ -0,0 +1,5 @@
+const btn_1 = document.querySelector('.more-se');
+console.log(btn_1);
+btn_1.addEventListener('click',function(){
+    
+})
